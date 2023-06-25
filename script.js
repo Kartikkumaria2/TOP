5d65873f15b7b61cc624dd7eb07704a552a8ca5a
kartikkumaria2 <kkumaria_be21@thapar.edu>
2023-06-25 13:36:31 +0530

    Edit index.html

--- script.js
+++ script.js
@@ -15,16 +15,40 @@ function getuser(){
     return b;
 }
 function play(user,comp){
+    user  = user.toLowerCase()
+    let result= ''
     if(user == comp){
-        return 'draw';
+        result = 'draw';
     }
     else if((user=='scissors'&&comp=='paper')||(user=='rock'&&comp=='scissors')||(user=='paper'&comp=='rock')){
-        return 'user wins';
+        result = 'user wins';
     }
     else{
-        return 'computer wins';
+        result = 'computer wins';
     }
+
+    return result;
+    
+
 }
-let user =getuser();
-let comp = getComputer();
-console.log(play(user,comp))
+const container = document.querySelector('.userb')
+const div  = document.createElement('div');
+div.style.fontSize = '72px';
+div.style.flex = '1';
+div.style.height = '40%';
+    div.classList.add('new');
+    container.appendChild(div);
+const btns = document.querySelectorAll('.comm');
+btns.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+       let l = btn.innerHTML;
+       div.textContent =  play(l,getComputer());
+       
+       
+       
+    })
+}
+
+)
+
+
